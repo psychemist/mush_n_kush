997914ca00b5d2ed916e853aa44fa73fdb1c6ad2
frankie-powers <franklin.power21@gmaul.com>
2024-09-29 21:30:33 +0100

    updated the metadata

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,26 +1,27 @@
-import './globals.css'
-import { Poppins } from 'next/font/google'
+import "./globals.css";
+import { Poppins } from "next/font/google";
 
-const poppins = Poppins({ subsets: ['latin'], weight: ['400', '500', '600', '700', '900'] })
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "900"],
+});
 
 export const metadata = {
-  title: 'Construction - Build the future',
-  description: 'Construction Landing Page',
-  author: 'Edlávio Pedro Alberto',
-}
+  title: "Mush n'Kush - Nature's Finest",
+  description: "Landing Page",
+  author: "Serenity Vortex",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={`${poppins.className} bg-white text-gray-500`}>
-        <main className='mx-auto w-11/12 lg:w-10/12'>
-          {children}
-        </main>
+        <main className="mx-auto w-11/12 lg:w-10/12">{children}</main>
       </body>
     </html>
-  )
+  );
 }
